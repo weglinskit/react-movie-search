7427d17169908b7be1dc265b927e70451eb707a3
Tomasz Węgliński <weglinski.tomasz@gmail.com>
2017-09-27 13:23:55 +0200

    Too long description text abstraction on result list; Mock backdrop image if original one is not available;

--- src/scenes/Home/components/MoviesList/components/MovieBox/index.js
+++ src/scenes/Home/components/MoviesList/components/MovieBox/index.js
@@ -8,16 +8,38 @@ import React from 'react';
  * @return {Object} JSX object with short movie description layout
  */
 const MovieBox = (props) => {
+
+    /**
+     * Abstraction for too long text description.
+     *
+     * @method textAbstract
+     * @return {String} Abstracted text string
+     */
+    function textAbstract(text) {
+        text = text.substring(0, 300);
+        let last = text.lastIndexOf(" ");
+        text = text.substring(0, last);
+        return text + "...";
+    }
+
+    const abstractedDescription = props.movie.overview.length > 300 ?
+        textAbstract(props.movie.overview) :
+        props.movie.overview;
+
     return (
         <div className="col-md-6 col-lg-4">
             <div className="card">
                 <img className="card-img-top"
-                     src={ `${ props.imageBaseUrl }/${ props.backDropSize }/${ props.movie.backdrop_path }` }
+                     src={
+                        props.movie.backdrop_path ?
+                            `${ props.imageBaseUrl }/${ props.backDropSize }/${ props.movie.backdrop_path }` :
+                            'http://via.placeholder.com/300x169'
+                     }
                      alt="Card cap" />
                 <div className="card-body">
                     <h4 className="card-title">{ props.movie.title }</h4>
                     <p className="card-text">
-                        { props.movie.overview }
+                        { abstractedDescription }
                     </p>
                     <a href={ `/movie/${ props.movie.id }` } className="btn btn-primary">Go somewhere</a>
                 </div>
