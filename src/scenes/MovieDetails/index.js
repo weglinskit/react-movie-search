3416671bb9f1d85a5690ff1570b095198ca4dc6c
Tomasz Węgliński <weglinski.tomasz@gmail.com>
2017-09-28 23:36:14 +0200

    Movie detail page view; Icons lib added;

--- src/scenes/MovieDetails/index.js
+++ src/scenes/MovieDetails/index.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import { getMovieById, getConfiguration } from '../../services/movies/index';
 
 /**
  * MovieDetails component - movie detail page. This component provides basic view with movie details.
@@ -12,7 +15,10 @@ class MovieDetails extends Component {
     constructor() {
         super();
         this.state = {
-            movieId: null
+            movieId: null,
+            movieObject: {},
+            imageBaseUrl: '',
+            posterSize: ''
         };
     }
 
@@ -20,6 +26,27 @@ class MovieDetails extends Component {
         this.setState({
             movieId: this.props.match.params.id
         });
+
+        getConfiguration()
+            .then((json) => {
+                this.setState({
+                    imageBaseUrl: json.images.base_url,
+                    posterSize: json.images.poster_sizes[2]
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        getMovieById(this.props.match.params.id)
+            .then((json) => {
+                this.setState({
+                    movieObject: json
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     /**
@@ -30,7 +57,39 @@ class MovieDetails extends Component {
      */
     render() {
         return (
-            <p>Movie id: { this.state.movieId }</p>
+            <div className="container">
+                <nav className="breadcrumb mt-3">
+                    <Link to={ '/?back=true' } className="breadcrumb-item active">
+                        <span className="oi oi-arrow-circle-left mr-2"></span>
+                        Back to search
+                    </Link>
+                </nav>
+                <div className="row">
+                    <div className="col-sm-12">
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-8">
+                        <h1>{ this.state.movieObject.title }</h1>
+                        <h3>
+                            Rank
+                            <span className="badge badge-pill badge-primary ml-2">
+                                { this.state.movieObject.vote_average }
+                            </span>
+                        </h3>
+                        { this.state.movieObject.overview }
+                    </div>
+                    <div className="col-sm-4">
+                        <img className="img-fluid"
+                             src={
+                                this.state.movieObject.poster_path ?
+                                    `${ this.state.imageBaseUrl }/${ this.state.posterSize }/${ this.state.movieObject.poster_path }` :
+                                    'http://via.placeholder.com/185x278'
+                             }
+                             alt="Movie side" />
+                    </div>
+                </div>
+            </div>
         );
     }
 }
